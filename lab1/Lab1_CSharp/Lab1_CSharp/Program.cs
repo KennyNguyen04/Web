66f769ae81e16381a24617c294b078e5a64940c5
KennyNguyen04 <dungnguyen15112004@gmail.com>
2025-12-08 00:02:17 +0700

    lab 1

---
+++ lab1/Lab1_CSharp/Lab1_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// Bài tập: Write your first C# code
+using System;
+
+namespace Lab1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first C# code for Lab 1.");
+        }
+    }
+}
---
+++ lab1/Web_Exercise/app.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+console.log('Script loaded!');
+
+// Lấy phần tử nút bấm và thẻ p thông báo
+const btn = document.getElementById('clickMeBtn');
+const msg = document.getElementById('message');
+
+// Thêm sự kiện click
+btn.addEventListener('click', function () {
+    msg.textContent = "You have successfully clicked the button! JS is working.";
+    msg.style.color = "green";
+    console.log('Button clicked');
+});
